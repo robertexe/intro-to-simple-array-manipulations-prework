7fa312d1faf20f0f1cda466c4cefca5cfc4cf605
Robert Tabares <robtabares92@gmail.com>
2018-05-14 23:38:39 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -38,6 +38,10 @@ def using_flatten(arr)
   arr.flatten()
 end
 
-def using_delte(arr, str)
+def using_delete(arr, str)
   arr.delete(str)
+end
+
+def using_delete_at(arr, num)
+  arr.delete(num)
 end
