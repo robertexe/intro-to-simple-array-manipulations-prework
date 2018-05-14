8de91b08de8b3a9d0353f4f464fc8c569bf3d106
Robert Tabares <robtabares92@gmail.com>
2018-05-14 23:11:43 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,17 @@
+def using_push(arr, str)
+  arr.push(str)
+end
+
+def using_unshift(arr, str)
+    arr.unshift(str)
+end
+
+def using_pop(arr)
+  arr.pop()
+end
+
+def pops_with_args(arr, n)
+  n.times do
+    arr.pop()
+  end
+end
